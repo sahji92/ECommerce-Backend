9cd90f8bb803c0aa28ce8ad5e772e16bff5940f1
deepak sah <sahdeepak330@gmail.com>
2023-11-05 19:21:00 +0530

    integrating passport-jwt,created getusers, loginuser routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const { createUser } = require('../controllers/userController')
+const { createUser, loginUser, getUsers } = require('../controllers/userController')
+const passport = require('passport')
 
 const router = express.Router()
 
 router.post('/create-user',createUser)
+
+router.post('/login-user',loginUser)
+
+router.get('/get-users', passport.authenticate('jwt', {session: false}),getUsers)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express=require('express')
 const cors=require('cors')
 const mongoConnection=require('./connection');
-const routes = require('./routes/index')
+const routes = require('./routes/index');
+const { initializePassport } = require('./passport');
+const passport=require('passport')
 require('dotenv').config()
+
 
 const app=express();
 app.use(cors())
 mongoConnection(process.env.URI)
+//12-13--> defining stretegy of passport then initialize it in our App.
+//we are configuring(defining stretegy, etcc) in passport .js so we dont have to write
+//tedious code here in server.js
+initializePassport(passport);
+app.use(passport.initialize())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
